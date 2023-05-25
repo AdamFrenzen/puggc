dfa6d8008e0524fda84f700767af51fe25f347c8
AdamFrenzen <134336747+AdamFrenzen@users.noreply.github.com>
2023-05-24 22:00:10 -0600

    Added pugify file

---
+++ pugify.py
@@ -0,0 +1,46 @@
+import os
+
+
+def pugify():
+
+    folder = input("Paste the component folder's absolute path: ")
+    spec = input("Delete spec file? (y,n): ")
+
+    try:
+        os.listdir(folder)
+    except FileNotFoundError:
+        print("ERROR loading path")
+        return
+
+    for file in os.listdir(folder):
+        if "component.html" in file:
+            with open(os.path.join(folder, file), "w") as pug:
+                pug.truncate()
+
+            os.rename(os.path.join(folder, file), os.path.join(folder, (file.replace(".html", ".pug"))))
+
+        elif spec == "y" and "component.spec.ts" in file:
+            print(file, "removed")
+            os.remove(os.path.join(folder, file))
+
+        elif "component.ts" in file:
+            with open(os.path.join(folder, file), "r+") as code:
+
+                output = []
+
+                for line in code.readlines():
+                    if "templateUrl: " in line:
+                        output.append(line.replace(".html", ".pug"))
+                        # "  templateUrl: './comp.component.html',\n"
+                    else:
+                        output.append(line)
+
+                code.truncate(0)
+                code.seek(0)
+                code.writelines(output)
+
+    print("Pugification successful")
+
+
+if __name__ == '__main__':
+    pugify()
