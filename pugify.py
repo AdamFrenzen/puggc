82cf593fc8ec7a89e54a9f677ab51ecdabb3de50
adam <adam.frenzen@gmail.com>
2023-05-30 18:10:34 -0600

    modified the script to now keep the spec file and now remove the css file instead.

--- pugify.py
+++ pugify.py
@@ -4,7 +4,6 @@ import os
 def pugify():
 
     folder = input("Paste the component folder's absolute path: ")
-    spec = input("Delete spec file? (y,n): ")
 
     try:
         os.listdir(folder)
@@ -19,8 +18,8 @@ def pugify():
 
             os.rename(os.path.join(folder, file), os.path.join(folder, (file.replace(".html", ".pug"))))
 
-        elif spec == "y" and "component.spec.ts" in file:
-            print(file, "removed")
+        elif "component.css" in file or "component.scss" in file:
+            # with open(os.path.join(folder, file), "r") as style:
             os.remove(os.path.join(folder, file))
 
         elif "component.ts" in file:
@@ -32,8 +31,10 @@ def pugify():
                     if "templateUrl: " in line:
                         output.append(line.replace(".html", ".pug"))
                     else:
-                        output.append(line)
+                        if "styleUrls:" not in line:
+                            output.append(line)
 
+                print(output)
                 code.truncate(0)
                 code.seek(0)
                 code.writelines(output)
